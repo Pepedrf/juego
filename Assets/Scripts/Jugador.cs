2736539ceebd49fe2f21c8dc665797e4d087b360
pepedrf_ <pepedrf692@gmail.com>
2024-04-09 18:54:13 +0200

    Inicio

---
+++ Assets/Scripts/Jugador.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Jugador : MonoBehaviour
+{
+    
+    private Rigidbody rb;
+
+    public float speed = 5f;
+
+
+    private void Awake() {
+        rb= GetComponent<Rigidbody>();
+    }
+
+    private void Update() {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal*speed, rb.velocity.y, moveVertical*speed);
+
+        rb.velocity = movement;
+    }
+
+}
